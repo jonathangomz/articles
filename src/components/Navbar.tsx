4081b537ef555f8478e91144a0b85c208e023bbc
jonathangomz <jonicgp97@gmail.com>
2024-03-01 16:10:55 -0600

    Fixes Navbar overflow

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,9 +12,11 @@ export default function Navbar({title, buttonLink, buttonText}: {
 
   return (
     <NavbarContainer>
-      <LinkButton to={buttonLink}>{buttonText}</LinkButton>
-      <Title>{title}</Title>
-      <Button type='danger' onClick={auth.logout}>Logout</Button>
+      <NavbarLayout>
+        <LinkButton to={buttonLink}>{buttonText}</LinkButton>
+        <Title>{title}</Title>
+        <Button type='danger' onClick={auth.logout}>Logout</Button>
+      </NavbarLayout>
     </NavbarContainer>
   );
 };
@@ -26,12 +28,15 @@ const NavbarContainer = styled.div`
   width: 100%;
   background-color: #333;
   color: #fff;
-  padding: 10px 50px;
   z-index: 999;
+`;
+
+const NavbarLayout = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   gap: 12px;
+  margin: 10px;
 `;
 
 const Title = styled.h3`
